301ca61ddde11135be9e984a598944ae3fec086d
Vonschell <vonschell02@gmail.com>
2024-12-13 21:09:20 +0900

    Added an event listener for the guess button

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -1,11 +1,11 @@
-const guessedLettersElement = document.querySelector (".guessed-letters");
-const guessButton = document.querySelector (".guess");
-const letterInput = document.querySelector (".letter");
-const wordInProgress = document.querySelector (".word-in-progress");
-const remainingGuessesElement = document.querySelector (".remaining");
-const remainingGuessesSpan = document.querySelector (".remaining span");
-const messages = document.querySelector (".message");
-const playAgainButton = document.querySelector (".play-again");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const messages = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 
@@ -20,3 +20,10 @@ const placeholders = function (word) {
 };
 
 placeholders(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
