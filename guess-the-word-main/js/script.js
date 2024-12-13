96328c2a1c5f2f7fff0c60f4f33671c16802f549
Vonschell <vonschell02@gmail.com>
2024-12-13 20:39:26 +0900

    Added global variables

---
+++ guess-the-word-main/js/script.js
@@ -0,0 +1,10 @@
+const guessedLettersElement = document.querySelector (".guessed-letters");
+const guessButton = document.querySelector (".guess");
+const letterInput = document.querySelector (".letter");
+const wordInProgress = document.querySelector (".word-in-progress");
+const remainingGuessesElement = document.querySelector (".remaining");
+const remainingGuessesSpan = document.querySelector (".remaining span");
+const messages = document.querySelector (".message");
+const playAgainButton = document.querySelector (".play-again");
+
+const word = "magnolia";
